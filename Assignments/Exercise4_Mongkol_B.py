78ebb83243663d5843d3eeac2a4a0b12f18ec035
Monkol39541 <65963914+Monkol39541@users.noreply.github.com>
2020-05-28 11:43:35 +0700

    Assignments

---
+++ Assignments/Exercise4_Mongkol_B.py
@@ -0,0 +1,9 @@
+FE = 76.0
+GB = 65.4
+ICS = 92.5
+CP = 84.3
+print("----------Your Score----------")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
