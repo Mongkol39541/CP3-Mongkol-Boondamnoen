c32c98a6818e575fb58105b0e28a96f780a56190
Monkol39541 <65963914+Monkol39541@users.noreply.github.com>
2020-06-09 09:54:07 +0700

    def

---
+++ Assignments/Text Pyton/Lecture 49_Mongkol_Boondamnoen.py
@@ -0,0 +1,10 @@
+def SayHello():
+    print("Hello Word")
+    Name()
+    print("Woooooo")
+def Name():
+    print("Mongkol  Boondamnoen")
+    print("Hahaha")
+    SayHello()
+SayHello()
+Name()
---
+++ Assignments/Text Pyton/Lecture 50_Mongkol_Boondamnoen.py
@@ -0,0 +1,12 @@
+def Total(x,y):
+    print(x+y)
+def Difference(x,y):
+    print(x-y)
+def Multiple(x,y):
+    print(x*y)
+def Quotient(x,y):
+    print(x/y)
+Total(20,70)
+Difference(200,175)
+Multiple(25,4)
+Quotient(300,15)
